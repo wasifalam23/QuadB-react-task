e73ec62e7584a7a6a77453cef957b77a95143f79
wasif_alam <wsfalam84@gmail.com>
2023-08-07 23:57:57 +0530

    fixed navbar again

--- src/components/NabBar.tsx
+++ src/components/NabBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className="py-4 bg-teal-600">
       <NavLink to="/">
-        <h3 className="text-2xl px-4 text-gray-100 font-medium ml-12 max-md:px-6 max-md:ml-0">
+        <h3 className="text-2xl px-6 text-gray-100 font-medium ml-12  max-xl:ml-0">
           ShowBox
         </h3>
       </NavLink>
